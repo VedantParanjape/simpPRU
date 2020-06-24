33753818f989849fe236df2aa6d097bdcf2d640b
vedantparanjape <22630228+VedantParanjape@users.noreply.github.com>
2020-06-24 15:12:33 +0530

    Added semantic checker for return statements and return types in functions

--- frontend/semantic.c
+++ frontend/semantic.c
@@ -37,3 +37,33 @@ int check_function_call(ast_node_function_call *function_call)
 
     return -1;
 }
+
+int check_function_definition(ast_node_function_def *function_defs)
+{
+    int dt = 0;
+
+    if (function_defs->symbol_entry != NULL)
+    {
+        if (function_defs->return_statment == NULL)
+        {
+            dt = DT_VOID_;
+        }
+        else if (function_defs->return_statment->node_type == AST_NODE_ARITHMETIC_EXP)
+        {
+            dt = DT_INTEGER;
+        }
+        else
+        {
+            dt = DT_BOOLEAN;
+        }
+
+        if (function_defs->symbol_entry->data_type != dt)
+        {
+            return -1;
+        }
+
+        return 1;       
+    }
+
+    return -1;
+}
