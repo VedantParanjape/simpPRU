78dd3293b0b296ee9672ea249d47af0ddab0a236
vedantparanjape <22630228+VedantParanjape@users.noreply.github.com>
2020-07-18 02:04:51 +0530

    Added pru_ecap.h with bit fields defined by me, and address def fixed for gnugcc

--- generated_code/include/am335x/pru_ecap.h
+++ generated_code/include/am335x/pru_ecap.h
@@ -94,23 +94,107 @@ typedef struct {
 	uint32_t rsvd118[4];		// 0x118 - 0x124
 
 	/* PRU_ECAP_ECCTL1 register bit field */
-	volatile uint16_t ECCTL1; // 0x28
+	union { 
+        volatile uint16_t ECCTL1; // 0x28
+
+        volatile struct {
+            unsigned CAP1POL  : 1; // 0
+            unsigned CTRRST1  : 1; // 1
+            unsigned CAP2POL  : 1; // 2
+            unsigned CTRRST2  : 1; // 3
+            unsigned CAP3POL  : 1; // 4
+            unsigned CTRRST3  : 1; // 5
+            unsigned CAP4POL  : 1; // 6
+            unsigned CTRRST4  : 1; // 7
+            unsigned CAPLDEN  : 1; // 8
+            unsigned PRESCALE : 5; // 13:9
+            unsigned FREE_SOFT : 2; // 15:14 
+        } ECCTL1_bit;
+    }
 
 	/* PRU_ECAP_ECCTL2 register bit field */
-	volatile uint16_t ECCTL2;	// 0x2A
+	union {
+        volatile uint16_t ECCTL2;	// 0x2A
+
+        volatile struct {
+            unsigned CONT_ONESHT : 1; // 0
+            unsigned STOP_WRAP   : 2; // 2:1
+            unsigned REARM       : 1; // 3
+            unsigned TSCTRSTOP   : 1; // 4
+            unsigned SYNCI_EN    : 1; // 5
+            unsigned SYNCO_SEL   : 2; // 7:6
+            unsigned SWSYNC      : 1; // 8
+            unsigned CAP_APWM    : 1; // 9
+            unsigned APWMPOL     : 1; // 10
+            unsigned RESERVED0   : 5; // 15:11
+        } EECTL2_bit;
+    }
 
 	/* PRU_ECAP_ECEINT register bit field */
-	volatile uint16_t ECEINT;	// 0x2C
+	union {
+        volatile uint16_t ECEINT;	// 0x2C
 
+        volatile struct {
+            unsigned RESERVED0 : 1; // 0
+            unsigned CEVT1     : 1; // 1
+            unsigned CEVT2     : 1; // 2
+            unsigned CEVT3     : 1; // 3
+            unsigned CEVT4     : 1; // 4
+            unsigned CTROVF    : 1; // 5
+            unsigned CTR_PRD   : 1; // 6
+            unsigned CTR_CMP   : 1; // 7
+            unsigned RESERVED1 : 8; // 15:8
+        } ECEINT_bit;
+    }
 	/* PRU_ECAP_ECFLG register bit field */
-	volatile uint16_t ECFLG;	// 0x2E
+	union {
+        volatile uint16_t ECFLG;	// 0x2E
 
+        volatile struct {
+            unsigned INT       : 1; // 0
+            unsigned CEVT1     : 1; // 1
+            unsigned CEVT2     : 1; // 2
+            unsigned CEVT3     : 1; // 3
+            unsigned CEVT4     : 1; // 4
+            unsigned CTROVF    : 1; // 5
+            unsigned CTR_PRD   : 1; // 6
+            unsigned CTR_CMP   : 1; // 7
+            unsigned RESERVED0 : 8; // 15:8
+        } ECFLG_bit;
+    }
 	/* PRU_ECAP_ECCLR register bit field */
-	volatile uint16_t ECCLR;	// 0x30
+	union {
+        volatile uint16_t ECCLR;	// 0x30
+        
+        volatile struct {
+            unsigned INT       : 1; // 0
+            unsigned CEVT1     : 1; // 1
+            unsigned CEVT2     : 1; // 2
+            unsigned CEVT3     : 1; // 3
+            unsigned CEVT4     : 1; // 4
+            unsigned CTROVF    : 1; // 5
+            unsigned CTR_PRD   : 1; // 6
+            unsigned CTR_CMP   : 1; // 7
+            unsigned RESERVED0 : 8; // 15:8
+        } ECCLR_bit;
+    }
 
 	/* PRU_ECAP_ECFRC register bit field */
-	volatile uint16_t ECFRC;	// 0x32
+	union {
+        volatile uint16_t ECFRC;	// 0x32
 
+        volatile struct {
+            unsigned RESERVED0 : 1; // 0
+            unsigned CEVT1     : 1; // 1
+            unsigned CEVT2     : 1; // 2
+            unsigned CEVT3     : 1; // 3
+            unsigned CEVT4     : 1; // 4
+            unsigned CTROVF    : 1; // 5
+            unsigned CTR_PRD   : 1; // 6
+            unsigned CTR_CMP   : 1; // 7
+            unsigned RESERVED1 : 8; // 15:8
+        } ECFRC_bit;
+    }
 	uint32_t rsvd34[10];		// 0x34 - 0x58
 
 	/* PRU_ECAP_REVID register bit field */
@@ -123,6 +207,11 @@ typedef struct {
 	};	// 0x5C
 } pruEcap;
 
+#ifdef __GNUC__
+static volatile pruEcap *__CT_ECAP = (void*)0x00030000;
+#define CT_ECAP (*__CT_ECAP)
+#else
 volatile __far pruEcap CT_ECAP __attribute__((cregister("PRU_ECAP", near), peripheral));
+#endif
 
 #endif /* _PRU_ECAP_H_ */
