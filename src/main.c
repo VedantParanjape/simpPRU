f89583154f159f942bb442d837fa55e230dbab31
vedantparanjape <22630228+VedantParanjape@users.noreply.github.com>
2020-08-03 23:40:35 +0530

    Fixed a bug in passing of static library to linked, removed lib from name

--- src/main.c
+++ src/main.c
@@ -163,7 +163,7 @@ int main(int argc, char** argv)
     char command[700];
     if (is_rpmsg_used == 1)
     {
-        snprintf(command, 700, "pru-gcc /tmp/temp.c -L%s/lib/ -llibprurpmsg%d -o %s.pru%d -mmcu=am335x.pru%d -I%s/include/pru/  -DCONFIG_ENABLE_RPMSG=1", TOSTRING(INSTALL_PATH), arguments.pruid, arguments.output_filename, arguments.pruid, arguments.pruid, TOSTRING(INSTALL_PATH));
+        snprintf(command, 700, "pru-gcc /tmp/temp.c -L%s/lib/ -lprurpmsg%d -o %s.pru%d -mmcu=am335x.pru%d -I%s/include/pru/  -DCONFIG_ENABLE_RPMSG=1", TOSTRING(INSTALL_PATH), arguments.pruid, arguments.output_filename, arguments.pruid, arguments.pruid, TOSTRING(INSTALL_PATH));
         if (system(command) == -1)
         {
             fprintf(stderr, "\e[31mfatal error:\e[0m unable to call pru-gcc\n");
