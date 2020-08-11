d25428704a25d66064cefbc3ec47bb4873ca3e92
vedantparanjape <22630228+VedantParanjape@users.noreply.github.com>
2020-08-11 19:15:56 +0530

    * Fixed issues with console:    1) Hot reload was not working, inshort, app needed to be restarted to after stopping PRU it couldn't be started again.    2) Screen wasn't being refreshed on adding elements to outputbox.    3) Receiver thread wasn't terminated on stopping PRU, fixed this bug, and used atomic variables for vars shared between two threads.    4) Fixed concurrency issues, due to which race condition was occuring.

--- include/console/console.hpp
+++ include/console/console.hpp
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <thread>
 #include <mutex>
-#include <future>
+#include <atomic>
 #include <chrono>
 
 #include <ftxui/dom/elements.hpp>
@@ -22,6 +22,25 @@
 #define MODEL_BEAGLEBONE_BLACK 2
 #define MODEL_BEAGLEBONE_BLACK_WIRELESS 3
 #define MODEL_BEAGLEBONE_AI 4
+
+using namespace ftxui;
+class console : public Component 
+{
+    public:
+        console(); 
+        
+    private:
+        Container container = Container::Horizontal();
+        RadioBox right_menu;
+        Input input_box;
+        Toggle pru_start_top;
+        Elements output_box;
+        int pru_id = 0;
+        int data_sent = 0;
+        int started = -1;
+        
+        Element Render() override;
+};
 
 int device_model();
 int send_rpmsg_data(int value, int pru_id);
--- src/console/console.cpp
+++ src/console/console.cpp
@@ -7,10 +7,10 @@
 static int model_beaglebone__ = 0;
 static int rpmsg_fd = -1;
 std::thread read_rpmsg_thread;
-// std::promise<void> stop_read_signal;
-// std::future<void> break_flag;
-std::timed_mutex rpsmg_mutex;
-bool stop_read_signal_ = false;
+std::timed_mutex rpmsg_mutex;
+std::mutex output_box_mutex;
+std::atomic_bool stop_read_signal(false);
+std::atomic_bool update_screen(false);
 FILE *fd = NULL;
 
 int device_model()
@@ -30,7 +30,11 @@ int device_model()
         model_beaglebone__ = MODEL_BEAGLEBONE_BLACK_WIRELESS;
         printf("Detected %s\n", model_name);
     }
-    // Add BBB also
+    else if (!strcmp(model_name, "TI AM335x BeagleBone Black"))
+    {
+        model_beaglebone__ = MODEL_BEAGLEBONE_BLACK;
+        printf("Detected %s\n", model_name);
+    }
     else if (!strcmp(model_name, "TI AM335x PocketBeagle"))
     {
         model_beaglebone__ = MODEL_POCKETBEAGLE;
@@ -68,16 +72,16 @@ int send_rpmsg_data(int value, int pru_id)
 
     const char* data = std::to_string(value).c_str();
 
-    std::unique_lock<std::timed_mutex> rpsmg_lock(rpsmg_mutex, std::defer_lock);
+    std::unique_lock<std::timed_mutex> rpmsg_lock(rpmsg_mutex, std::defer_lock);
 
-    if (rpsmg_lock.try_lock_for(std::chrono::milliseconds(1000)))
+    if (rpmsg_lock.try_lock_for(std::chrono::milliseconds(1000)))
     {
         fprintf(fd, "%s", "fd locked\n");
 
         int data_sent = write(rpmsg_fd, data, sizeof(char)*strlen(data));
-        fprintf(fd, "%s: %d\n", data, strlen(data));
+        fprintf(fd, "%s: %d\n", data, (int)strlen(data));
 
-        rpsmg_lock.unlock();
+        rpmsg_lock.unlock();
         fprintf(fd, "%s", "fd unlocked\n");
 
         close(rpmsg_fd);
@@ -89,9 +93,8 @@ int send_rpmsg_data(int value, int pru_id)
 }
 
 int receive_rpmsg_data(int pru_id, ftxui::Elements &output)
-{
-    // while(flag.wait_for(std::chrono::milliseconds(10)) == std::future_status::timeout)
-    while(!stop_read_signal_)
+{    
+    while(!stop_read_signal.load())
     {
         char buffer[512];
         for (int i = 0; i < 512; i++)
@@ -100,31 +103,31 @@ int receive_rpmsg_data(int pru_id, ftxui::Elements &output)
         }
         
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
-        std::unique_lock<std::timed_mutex> rpsmg_lock(rpsmg_mutex, std::defer_lock);
-        if (rpsmg_lock.try_lock_for(std::chrono::milliseconds(1000)))
+        std::unique_lock<std::timed_mutex> rpmsg_lock(rpmsg_mutex, std::defer_lock);
+        if (rpmsg_lock.try_lock_for(std::chrono::milliseconds(1000)))
         {   
-            // fprintf(fd, "%s", "fd recv locked\n");
-
             int rpmsg_fd = -1;
-            while(rpmsg_fd < 0)
+            while(rpmsg_fd < 0 && !stop_read_signal.load())
             {
                 rpmsg_fd = pru_id == 0 ? open("/dev/rpmsg_pru30", O_RDWR | O_NONBLOCK) : open("/dev/rpmsg_pru31", O_RDWR | O_NONBLOCK);
             }
             if (rpmsg_fd < 0)
             {
                 fprintf(fd, "error recv: %s\n", strerror(errno));
+                close(rpmsg_fd);
             }
 
             int data_read = read(rpmsg_fd, buffer, 512);
-            rpsmg_lock.unlock();
+            rpmsg_lock.unlock();
             
-            // fprintf(fd, "%s", "fd recv unlocked\n");
-
             if (data_read > 0)
             {
                 buffer[strlen(buffer)-1] = '\0';
-                // output.push_back(ftxui::text(std::wstring(&buffer[0], &buffer[512])));
-                fprintf(fd, "recv: %s: %d\n", buffer, strlen(buffer));
+                update_screen.store(true, std::memory_order_relaxed);
+                fprintf(fd, "recv: %s: %d\n", buffer, (int)strlen(buffer));
+                
+                std::lock_guard<std::mutex> output_box_lock(output_box_mutex);
+                output.push_back(ftxui::text(std::wstring(&buffer[0], &buffer[strlen(buffer)])));                
             }
             close(rpmsg_fd);
         }
@@ -133,9 +136,7 @@ int receive_rpmsg_data(int pru_id, ftxui::Elements &output)
             fprintf(fd, "error recv: %s\n", "couldn't unlock mutex");
         }
     }
-
-    stop_read_signal_ = false;
-    // fprintf(fd, "error recv: %s\n", "promise failed");
+    stop_read_signal.store(false, std::memory_order_relaxed);
 
     return 1;
 }
@@ -226,120 +227,108 @@ int stop_pru(int pru_id)
 }
 
 using namespace ftxui;
-
-class console : public Component 
+console::console() 
 {
-    public:
-        console() 
+    Add(&container);
+    container.Add(&right_menu);
+    container.Add(&input_box);
+    container.Add(&pru_start_top);
+
+    if (model_beaglebone__ == MODEL_BEAGLEBONE_AI)
+    {
+        right_menu.entries = {
+            L"PRU0-0", L"PRU0-1", L"PRU1-0", L"PRU1-1",
+        };
+    }
+    else
+    {
+        right_menu.entries = {
+            L"PRU0", L"PRU1",
+        };
+    }
+    input_box.placeholder = L"type here";
+    pru_start_top.entries = {
+        L"Start", L"Stop"
+    };
+
+    right_menu.on_change = [this] {
+        pru_id = right_menu.selected;
+    };
+    input_box.on_enter = [this] {
+        try
         {
-            Add(&container);
-            container.Add(&right_menu);
-            container.Add(&input_box);
-            container.Add(&pru_start_top);
-
-            if (model_beaglebone__ == MODEL_BEAGLEBONE_AI)
+            data_sent = std::stoi(input_box.content);
+            input_box.content = L"";
+            if (started == 0)
             {
-                right_menu.entries = {
-                    L"PRU0-0", L"PRU0-1", L"PRU1-0", L"PRU1-1",
-                };
+                send_rpmsg_data(data_sent, pru_id);
             }
-            else
-            {
-                right_menu.entries = {
-                    L"PRU0", L"PRU1",
-                };
-            }
-            
-            input_box.placeholder = L"type here";
-            pru_start_top.entries = {
-                L"Start", L"Stop"
-            };
-
-            right_menu.on_change = [this] {
-                pru_id = right_menu.selected;
-            };
-            input_box.on_enter = [this] {
-                try
-                {
-                    data_sent = std::stoi(input_box.content);
-                    input_box.content = L"";
-                    send_rpmsg_data(data_sent, pru_id);
-                }
-                catch(const std::invalid_argument &err)
-                {
-                    std::cerr << err.what() << "\n";
-                } 
-            };
-            pru_start_top.on_enter = [this] {
-                started = pru_start_top.selected;
-
-                if (started == 0)
-                {
-                    if (start_pru(pru_id) == 1)
-                    {
-                    //     stop_read_signal = std::promise<void>();
-                    //     break_flag = stop_read_signal.get_future();
-
-                        read_rpmsg_thread = std::thread(receive_rpmsg_data, pru_id, std::ref(output_box));
-                        read_rpmsg_thread.detach();
-                    }
-                }
-                else if (started == 1)
-                {
-                    if (stop_pru(pru_id) == 1)
-                    {
-                        // stop_read_signal.set_value();
-                        stop_read_signal_ = true;
-                        output_box.clear();
-                    }
-                }
-            };
         }
+        catch(const std::invalid_argument &err)
+        {
+            std::cerr << err.what() << "\n";
+        } 
+    };
+    pru_start_top.on_enter = [this] {
+        started = pru_start_top.selected;
+
+        if (started == 0)
+        {
+            if (start_pru(pru_id) == 1)
+            {
+                send_rpmsg_data(-1, pru_id);
+
+                read_rpmsg_thread = std::thread(receive_rpmsg_data, pru_id, std::ref(output_box));
+                read_rpmsg_thread.detach();
+            }
+        }
+        else if (started == 1)
+        {
+            if (stop_pru(pru_id) == 1)
+            {
+                stop_read_signal.store(true, std::memory_order_relaxed);
+                output_box.clear();
+            }
+        }
+    };
+}
   
-    private:
-        Container container = Container::Horizontal();
-        RadioBox right_menu;
-        Input input_box;
-        Toggle pru_start_top;
-        Elements output_box;
-        int pru_id = 0;
-        int data_sent = 0;
-        int started = -1;
-        
-        Element Render() override 
-        {
-            return border(vbox({
-                // Console and PRU selection
-                hbox({
-                    hbox({
-                        vbox({
-                            vbox({output_box}),
-                            text(L"   ") | ftxui::select,
-                        }) | frame,                                        
-                    }) | flex | border,
-                    vbox({
-                        hcenter(bold(text(L"PRU"))),
-                        separator(),
-                        right_menu.Render(),
-                    }) | border,
-                }) | flex,
-                
-                // Input box and PRU start/stop
+Element console::Render() 
+{
+    std::lock_guard<std::mutex> output_box_lock(output_box_mutex);
+    
+    return border(vbox({
+        // Console and PRU selection
+        hbox({
+            hbox({
                 vbox({
-                    hbox({
-                        text(L" send : "),
-                        input_box.Render(),
-                        separator(),
-                        pru_start_top.Render(),
-                    }),
-                }) | border,
-            }));
-        }
-};
+                    vbox({output_box}),
+                    text(L"   ") | ftxui::select,
+                }) | frame,                                        
+            }) | flex | border,
+            vbox({
+                hcenter(bold(text(L"PRU"))),
+                separator(),
+                right_menu.Render(),
+            }) | border,
+        }) | flex,
+        
+        // Input box and PRU start/stop
+        vbox({
+            hbox({
+                text(L" send : "),
+                input_box.Render(),
+                separator(),
+                pru_start_top.Render(),
+            }),
+        }) | border,
+    }));
+}
 
 int main(int argc, const char* argv[]) 
 {
     fd = fopen("/tmp/log.txt", "w+");
+
     int model = device_model();
     if (model == -1)
     {
@@ -347,23 +336,23 @@ int main(int argc, const char* argv[])
         // return 0;
     }
 
-    int init_rpmsg_0 = open("/dev/rpmsg_pru30", O_RDWR);
-    int init_rpmsg_1 = open("/dev/rpmsg_pru31", O_RDWR);
-
-    if (init_rpmsg_0 > 0)
-    {
-        const char* data = std::to_string(0).c_str();
-        int data_sent = write(rpmsg_fd, data, sizeof(char)*strlen(data));
-        close(init_rpmsg_0);
-    }
-    if (init_rpmsg_1 > 0)
-    {
-        const char* data = std::to_string(0).c_str();
-        int data_sent = write(rpmsg_fd, data, sizeof(char)*strlen(data));
-        close(init_rpmsg_1);
-    }
+    stop_pru(0);
+    stop_pru(1);
 
     auto screen = ScreenInteractive::Fullscreen();
+
+    std::thread update([&screen]() {
+    for (;;) {
+      using namespace std::chrono_literals;
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+      if(update_screen.load())
+      {
+          update_screen.store(false, std::memory_order_relaxed);
+          screen.PostEvent(Event::Custom);
+      }
+    }
+    });
+
     console console_simppru;
     screen.Loop(&console_simppru);
 }
