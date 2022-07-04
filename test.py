b0c9e632a3b40e3e4433260bde9697b4e5930426
Krishna-13-cyber <84722531+Krishna-13-cyber@users.noreply.github.com>
2022-07-04 14:51:39 +0530

    Added exit code for failure of tests (#38)

--- test.py
+++ test.py
@@ -21,9 +21,11 @@ except OSError:
 import subprocess
 from fnmatch import fnmatch
 import os
+import sys
 
 for file in os.listdir("tests/run_anywhere"):
     if fnmatch(file, "*.sim"):
+        print(f"    ")
         print("Running test ", file)
 
         transpile_output = subprocess.run(f"bin/simppru --preprocess -t tests/run_anywhere/{file}", shell=True, capture_output=True)
@@ -32,7 +34,7 @@ for file in os.listdir("tests/run_anywhere"):
             print(f"**** ****TEST FAILED**** ****: {file}")
             print("stdout:\n", transpile_output.stdout.decode("utf-8"), sep='')
             print("stderr:\n", transpile_output.stderr.decode("utf-8"), sep='')
-            continue
+            sys.exit(1)
 
         compile_output = subprocess.run(f"gcc -o tests/run_anywhere/{file}.out /tmp/temp.c", shell=True, capture_output=True)
 
@@ -40,7 +42,7 @@ for file in os.listdir("tests/run_anywhere"):
             print(f"**** ****TEST FAILED**** ****: {file}")
             print("stdout:\n", compile_output.stdout.decode("utf-8"), sep='')
             print("stderr:\n", compile_output.stderr.decode("utf-8"), sep='')
-            continue
+            sys.exit(1)
 
         run_output = subprocess.run(f"tests/run_anywhere/{file}.out", shell=True, capture_output=True)
 
@@ -48,14 +50,18 @@ for file in os.listdir("tests/run_anywhere"):
             print(f"**** ****TEST FAILED**** ****: {file}")
             print("stdout:\n", run_output.stdout.decode("utf-8"), sep='')
             print("stderr:\n", run_output.stderr.decode("utf-8"), sep='')
-            continue
+            sys.exit(1)
         else:
             output = run_output.stdout
-        
         with open(f"tests/run_anywhere/{file}.output", 'r+b') as out:
             expected_output = out.read()
 
         if output != expected_output:
             print(f"**** ****TEST FAILED**** ****: {file}")
+            print(f"Standard Output:{output}")
+            print(f"Expected Output:{expected_output}")
+            sys.exit(1)
         else:
             print(f"Test {file} passed")
+            print(f"Standard Output:{output}")
+            print(f"Expected Output:{expected_output}")
